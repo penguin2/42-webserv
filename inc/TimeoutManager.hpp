1780a720575951fa1f22ee40efe857dcb61aa85b
taekklee <taekklee@student.42tokyo.jp>
2024-04-02 05:53:19 +0900

    [add] header files for socket timeouts

---
+++ inc/TimeoutManager.hpp
@@ -0,0 +1,31 @@
+#ifndef WEBSERV_TIMEOUT_MANAGER_H
+#define WEBSERV_TIMEOUT_MANAGER_H
+
+#include <map>
+#include <vector>
+
+#include "Time.hpp"
+
+class ASocket;
+
+class TimeoutManager {
+ public:
+  TimeoutManager();
+  ~TimeoutManager();
+
+  void addTimeouts(std::vector<ASocket*>& timeouts) const;
+
+  int insert(ASocket* socket);
+  int update(ASocket* socket);
+  int erase(ASocket* socket);
+
+ private:
+  std::multimap<Time, ASocket*> timeout_logger_;
+
+  static const Time kDefaultTimeoutLimit;
+
+  TimeoutManager(const TimeoutManager&);
+  TimeoutManager& operator=(const TimeoutManager&);
+};
+
+#endif
