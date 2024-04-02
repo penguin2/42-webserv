cc9d1f2dbbe8053f0cd08e3181e891a32fd02b1b
Your Name <you@example.com>
2024-04-02 22:47:47 +0900

    fix c++98

--- srcs/config/parse.cpp
+++ srcs/config/parse.cpp
@@ -7,6 +7,10 @@
 
 class NginxConfigParser {
  public:
+  NginxConfigParser() {
+    current_context = DEFAULT;
+    current_delimiter = SPACE;
+  }
   void ParseConfig(const std::string& filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
@@ -16,14 +20,14 @@ class NginxConfigParser {
 
     std::string line;
     while (std::getline(file, line)) {
-        ParseLine(line);
+      ParseLine(line);
     }
   }
 
  private:
-  enum class Context { DEFAULT, HTTP, SERVER, LOCATION };
-
-  enum class Delimiter {
+  enum Context { DEFAULT, HTTP, SERVER, LOCATION };
+  Context current_context;
+  enum Delimiter {
     SPACE = ' ',
     LEFT_BRACE = '{',
     RIGHT_BRACE = '}',
@@ -31,8 +35,7 @@ class NginxConfigParser {
     STRING = '*'
   };
 
-  Context current_context = Context::DEFAULT;
-  Delimiter current_delimiter = Delimiter::SPACE;
+  Delimiter current_delimiter;
 
   void ParseLine(const std::string& line) {
     if (line.empty() || line[0] == '#') {
@@ -43,19 +46,21 @@ class NginxConfigParser {
     Tokenize(line, tokens);
 
     if (tokens[0] == "events") {
-      current_context = Context::DEFAULT;
+      current_context = DEFAULT;
     } else if (tokens[0] == "http") {
-      current_context = Context::HTTP;
+      current_context = HTTP;
     } else if (tokens[0] == "server") {
-      current_context = Context::SERVER;
+      current_context = SERVER;
     } else if (tokens[0] == "location") {
-      current_context = Context::LOCATION;
+      current_context = LOCATION;
     } else if (tokens[0] == "}") {
-      if (current_context != Context::DEFAULT) {
+      if (current_context != DEFAULT) {
         current_context =
             static_cast<Context>(static_cast<int>(current_context) - 1);
       } else {
-        throw std::invalid_argument("Unexpected '}' outside any block");
+        std::cerr << "syntax error : Unexpected '}' outside any block"
+                  << std::endl;
+        exit(1);
       }
     } else {
       HandleDirective(tokens);
@@ -63,14 +68,15 @@ class NginxConfigParser {
   }
 
   void HandleDirective(const std::vector<std::string>& tokens) {
+    // TODO contextに合っていない要素についてはすべて文法エラーとする
     switch (current_context) {
-      case Context::HTTP:
+      case HTTP:
         std::cout << "Inside HTTP block: " << tokens[0] << std::endl;
         break;
-      case Context::SERVER:
+      case SERVER:
         std::cout << "Inside server block: " << tokens[0] << std::endl;
         break;
-      case Context::LOCATION:
+      case LOCATION:
         std::cout << "Inside location block: " << tokens[0] << std::endl;
         break;
       default:
@@ -83,22 +89,23 @@ class NginxConfigParser {
     std::istringstream iss(line);
     std::string token;
     char c;
+    // TODO delimiterの前がおかしいものはすべて文法エラーとする
     while (iss.get(c)) {
       switch (c) {
-        case static_cast<char>(Delimiter::SPACE):
-          this->current_delimiter = Delimiter::SPACE;
+        case static_cast<char>(SPACE):
+          this->current_delimiter = SPACE;
           break;
-        case static_cast<char>(Delimiter::LEFT_BRACE):
-          this->current_delimiter = Delimiter::LEFT_BRACE;
+        case static_cast<char>(LEFT_BRACE):
+          this->current_delimiter = LEFT_BRACE;
           break;
-        case static_cast<char>(Delimiter::RIGHT_BRACE):
-          this->current_delimiter = Delimiter::RIGHT_BRACE;
+        case static_cast<char>(RIGHT_BRACE):
+          this->current_delimiter = RIGHT_BRACE;
           break;
-        case static_cast<char>(Delimiter::SEMICOLON):
-          this->current_delimiter = Delimiter::SEMICOLON;
+        case static_cast<char>(SEMICOLON):
+          this->current_delimiter = SEMICOLON;
           break;
         default:
-          this->current_delimiter = Delimiter::STRING;
+          this->current_delimiter = STRING;
           break;
       }
 
@@ -118,6 +125,6 @@ class NginxConfigParser {
 
 int main() {
   NginxConfigParser parser;
-  parser.ParseConfig("nginx.conf");
+  parser.ParseConfig("sample.conf");
   return 0;
 }
