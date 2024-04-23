8ad40e3dc67f6cfbb1a5cdb5af1512d6b1f4b69b
Your Name <you@example.com>
2024-04-23 14:20:26 +0000

    fix

--- srcs/config/IndexDirectiveHandler.cpp
+++ srcs/config/IndexDirectiveHandler.cpp
@@ -13,6 +13,11 @@ void IndexDirectiveHandler::setConfig(long unsigned int server_num,
                                              std::string location_path) {
   Config& config = Config::getInstance();
   ServerConfig serverConfig = config.getServer(server_num);
+  if (!serverConfig.hasLocationConfig(location_path)) {
+    LocationConfig* newLocationConfig = new LocationConfig();
+    serverConfig.addLocationConfig(location_path, *newLocationConfig);
+  }
+
   LocationConfig locationConfig = serverConfig.getLocationConfig(location_path);
   std::cout << "setting : " << this->tokens[0] << std::endl;
   std::cout << "server num : " << server_num << std::endl;
