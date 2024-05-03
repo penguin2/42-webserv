be1b79d0789eac18aecfd57430e04823bc758659
rikeda <rikeda@penguin>
2024-05-03 10:59:49 +0900

    [update] ConfigAdapterのrandom要素を廃止

--- srcs/config/ConfigAdapter.cpp
+++ srcs/config/ConfigAdapter.cpp
@@ -8,7 +8,6 @@ const std::string *ConfigAdapter::searchRedirectUri(const std::string &host,
   static const std::string mock_redirect_uri(
       "/var/www/html/new-location/index.html");
 
-  if (rand() % 2 == 0) return &mock_redirect_uri;
   return NULL;
   (void)host;
   (void)port;
@@ -18,32 +17,7 @@ const std::string *ConfigAdapter::searchRedirectUri(const std::string &host,
 int ConfigAdapter::searchRedirectStatusCode(const std::string &host,
                                             size_t port,
                                             const std::string &path) {
-  int random_number = (rand() % 6);
-  int status_code;
-  switch (random_number) {
-    case 0:
-      status_code = 301;
-      break;
-    case 1:
-      status_code = 302;
-      break;
-    case 2:
-      status_code = 303;
-      break;
-    case 3:
-      status_code = 307;
-      break;
-    case 4:
-      status_code = 308;
-      break;
-    case 5:
-      status_code = 0;
-      break;
-    default:
-      status_code = 0;
-      break;
-  }
-  return status_code;
+  return 301;
   (void)host;
   (void)port;
   (void)path;
@@ -51,7 +25,7 @@ int ConfigAdapter::searchRedirectStatusCode(const std::string &host,
 
 bool ConfigAdapter::isCgiPath(const std::string &host, size_t port,
                               const std::string &path) {
-  return (rand() % 2 == 0);
+  return false;
   (void)host;
   (void)port;
   (void)path;
@@ -62,8 +36,7 @@ const std::string *ConfigAdapter::searchErrorPage(const std::string &host,
                                                   size_t status_code) {
   static const std::string mock_error_page("/var/www/html/error/error.html");
 
-  if (rand() % 2 == 0) return &mock_error_page;
-  return NULL;
+  return &mock_error_page;
   (void)host;
   (void)port;
   (void)status_code;
@@ -72,7 +45,7 @@ const std::string *ConfigAdapter::searchErrorPage(const std::string &host,
 bool ConfigAdapter::isAllowMethods(const std::string &host, size_t port,
                                    const std::string &path,
                                    const std::string &method) {
-  return (rand() % 2 == 0);
+  return true;
   (void)host;
   (void)port;
   (void)path;
@@ -83,9 +56,9 @@ std::vector<std::string> ConfigAdapter::getAllowMethods(
     const std::string &host, size_t port, const std::string &path) {
   std::vector<std::string> allow_methods;
 
-  if (rand() % 2 == 0) allow_methods.push_back("GET");
-  if (rand() % 2 == 0) allow_methods.push_back("POST");
-  if (rand() % 2 == 0) allow_methods.push_back("DELETE");
+  allow_methods.push_back("GET");
+  allow_methods.push_back("POST");
+  allow_methods.push_back("DELETE");
   return allow_methods;
   (void)host;
   (void)port;
@@ -99,19 +72,20 @@ bool ConfigAdapter::isCorrespondingMethod(const std::string &method) {
 std::vector<std::string> ConfigAdapter::makeAbsolutePaths(
     const std::string &host, size_t port, const std::string &path) {
   const std::string base_dir("/var/www/html");
-  const std::string base_cgi_dir("/lib");
   std::vector<std::string> absolute_paths;
 
-  if (path.compare(0, 9, "/cgi-bin/") == 0) {
-    absolute_paths.push_back(base_cgi_dir + path);
-  } else if (path == "" || path == "/") {
-    absolute_paths.push_back(base_dir + "/index.html");
-    absolute_paths.push_back(base_dir + "/app/index.html");
-    absolute_paths.push_back(base_dir + "/form/index.html");
+  if (host == "DELETE") {
+    absolute_paths.push_back(base_dir + path + "/");
+    absolute_paths.push_back(base_dir + path);
+
+  } else if (host == "POST") {
+    absolute_paths.push_back(base_dir + path);
+    absolute_paths.push_back(base_dir + path + "/");
   } else {
     absolute_paths.push_back(base_dir + path);
-    absolute_paths.push_back(base_dir + "/app" + path);
-    absolute_paths.push_back(base_dir + "/form" + path);
+    absolute_paths.push_back(base_dir + path + "/");
+    absolute_paths.push_back(base_dir + path + "/index.html");
+    absolute_paths.push_back(base_dir + "/index.html");
   }
   return absolute_paths;
   (void)host;
@@ -120,7 +94,7 @@ std::vector<std::string> ConfigAdapter::makeAbsolutePaths(
 
 bool ConfigAdapter::isAutoindexOn(const std::string &host, size_t port,
                                   const std::string &path) {
-  return (rand() % 2 == 0);
+  return true;
   (void)host;
   (void)port;
   (void)path;
@@ -153,36 +127,3 @@ size_t ConfigAdapter::getMaxMethodSize(void) {
 size_t ConfigAdapter::getMaxNumberOfCrlfBeforeMethod(void) {
   return INTERNAL::DEFAULT_MAX_NUMBER_OF_CRLF_BEFORE_METHOD;
 }
-
-// #include <iostream>
-// using namespace ConfigAdapter;
-// using namespace std;
-// int main(void) {
-//   srand(time(NULL));
-//   const std::string *uri = searchRedirectUri("", 0, "");
-//   cout << (uri ? *uri : "NULL") << endl;
-//   cout << searchRedirectStatusCode("", 0, "") << endl;
-//   cout << isCgiPath("", 0, "") << endl;
-//   const std::string *page = searchErrorPage("", 0, 400);
-//   cout << (page ? *page : "NULL") << endl;
-//   cout << isAllowMethods("", 0, "", "") << endl;
-//   vector<string> methods = getAllowMethods("", 0, "");
-//   cout << (1 <= methods.size() ? methods[0] : "")
-//        << (2 <= methods.size() ? ", " + methods[1] : "")
-//        << (3 <= methods.size() ? ", " + methods[2] : "");
-//   cout << endl;
-//   vector<string> paths = makeAbsolutePaths("", 0, "/");
-//   for (vector<string>::const_iterator it = paths.begin(); it != paths.end();
-//        ++it)
-//     cout << *it << " ";
-//   cout << endl;
-//   vector<string> cgi_paths = makeAbsolutePaths("", 0, "/cgi-bin/cgi.py");
-//   for (vector<string>::const_iterator it = cgi_paths.begin();
-//        it != cgi_paths.end(); ++it)
-//     cout << *it << " ";
-//   cout << endl;
-//   cout << getMaxHeaderSize() << endl;
-//   cout << getMaxUriSize() << endl;
-//   cout << getMaxBodySize("", 0, "") << endl;
-//   return 0;
-// }
