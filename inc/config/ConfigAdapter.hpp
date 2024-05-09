00445bf7cc3bf22ee23343dfdfd1e5a9e64793aa
rikeda-cloud <longsichitian648@gmail.com>
2024-05-09 17:51:36 +0900

    [update] ConfigAdapter内でhostやportを使用した検索をServerConfigやLocationConfigを使用した検索に変更

--- inc/config/ConfigAdapter.hpp
+++ inc/config/ConfigAdapter.hpp
@@ -4,62 +4,68 @@
 #include <string>
 #include <vector>
 
+#include "LocationConfig.hpp"
+#include "ServerConfig.hpp"
 namespace ConfigAdapter {
+// 該当するServerConfigが無い場合はServerConfigs[0]を使用する
+const ServerConfig& searchServerConfig(const std::string& host, size_t port);
+
+// 該当するLocationConfigが無い場合、NULLを返す
+const LocationConfig* searchLocationConfig(const ServerConfig& server_conf,
+                                           const std::string& path);
+
+// rootディレクティブとURI.Pathからサーバ内の絶対アドレスを作成する
+std::string makeAbsolutePath(const LocationConfig& location_conf,
+                             const std::string& path);
 
 // (リダイレクト対象のRequest) ? redirect_URI : NULL
-const std::string* searchRedirectUri(const std::string& host, size_t port,
-                                     const std::string& path);
+const std::string* searchRedirectUri(const LocationConfig& location_conf);
 
 // (リダイレクト対象のRequest) ? redirect_status_code : 0
 // searchRedirectUri関数後に実行される想定のため、0が返ることは基本ない
-int searchRedirectStatusCode(const std::string& host, size_t port,
-                             const std::string& path);
+int searchRedirectStatusCode(const LocationConfig& location_conf);
 
-// (Locationにcgi_path&cgi_extがあり、PathがCGIファイル) ? true : false
-bool isCgiPath(const std::string& host, size_t port, const std::string& path);
+// (Locationにcgi_ext && PathがCGIファイル) ? true : false
+bool isCgiPath(const LocationConfig& location_conf, const std::string& path);
 
 // (error_pageに該当するStatusCodeが含まれる) ? error_page_path : NULL
 // NULLの場合、呼び出し元でプログラムで組み立てたHTMLを返す
-const std::string* searchErrorPage(const std::string& host, size_t port,
-                                   size_t status_code);
+const std::string* searchErrorPage(const ServerConfig& server_conf, int code);
 
-// (method in allow_methods) ? true : false
-// allow_methodsディレクティブが存在しない場合、全てのMethodが許容される
-// allow_methodsディレクティブが存在し、methodが許容されない場合405
-bool isAllowMethods(const std::string& host, size_t port,
-                    const std::string& path, const std::string& method);
+bool isAllowMethods(const LocationConfig& location_conf,
+                    const std::string& method);
 
-// StatusCode405の時のAllowヘッダの組み立てに使用する予定
-std::vector<std::string> getAllowMethods(const std::string& host, size_t port,
-                                         const std::string& path);
+// 主に405ステータスコードの時にAllowヘッダの値に使用
+std::vector<std::string> getAllowMethods(const LocationConfig& location_conf);
 
-bool isCorrespondingMethod(const std::string& method);
+// CgiRequestでSERVER_NAMEメタ変数作成時に使用
+// 無い場合は空文字列 or デフォルト値？
+const std::string* searchHostName(const ServerConfig& server_conf);
 
-// 静的ファイルの組み立てをする際に使用
-// try_files,root,index,Pathコンポーネントから絶対パスのvectorを組み立てる
-std::vector<std::string> makeAbsolutePaths(const std::string& host, size_t port,
-                                           const std::string& path);
+bool isAutoindex(const LocationConfig& location_conf);
 
-// Uriの最大サイズ(一般的には2000bytes)
-size_t getMaxUriSize(void);
-// Headerの最大サイズ(1つのHeaderの最大サイズを想定)
-size_t getMaxHeaderSize(void);
-// Headerの最大個数
-size_t getMaxNumberOfHeaders(void);
+// indexが存在すればlocationPath + index, なければ空文字列
+std::string searchIndex(const LocationConfig& location_conf);
 
-// ボディの最大サイズ(client_max_body_sizeが存在しない場合、defaultの値を返す)
-size_t getMaxBodySize(const std::string& host, size_t port,
-                      const std::string& path);
-
-size_t getMaxMethodSize(void);
+// POSTメソッドで最大値を確認する際に使用
+// client_max_body_sizeが設定されていない場合、DEFAULT_MAX_CLIENT_BODY_SIZEを返す
+size_t getClientMaxBodySize(const LocationConfig& location_conf);
 
 size_t getMaxNumberOfCrlfBeforeMethod(void);
+size_t getMaxMethodSize(void);
+size_t getMaxUriSize(void);
+size_t getMaxHeaderSize(void);
+size_t getMaxNumberOfHeaders(void);
+size_t getMaxBodySize(void);
+
+bool isCorrespondingMethod(const std::string& method);
 
 namespace INTERNAL {
 const size_t DEFAULT_MAX_URI_SIZE = 2000;
 const size_t DEFAULT_MAX_HEADER_SIZE = 200;
 const size_t DEFAULT_MAX_NUMBER_OF_HEADERS = 100;
 const size_t DEFAULT_MAX_BODY_SIZE = 10000;
+const size_t DEFAULT_MAX_CLIENT_BODY_SIZE = 1000;
 // (std::string("DELETE").size() == 6)
 const size_t DEFAULT_MAX_METHOD_SIZE = 6;
 const size_t DEFAULT_MAX_NUMBER_OF_CRLF_BEFORE_METHOD = 10;
--- srcs/config/ConfigAdapter.cpp
+++ srcs/config/ConfigAdapter.cpp
@@ -1,129 +1,104 @@
-#include "./config/ConfigAdapter.hpp"
+#include "config/ConfigAdapter.hpp"
 
-#include <cstdlib>
+#include "config/Config.hpp"
+#include "config/LocationConfig.hpp"
+#include "config/ServerConfig.hpp"
 
-const std::string *ConfigAdapter::searchRedirectUri(const std::string &host,
-                                                    size_t port,
-                                                    const std::string &path) {
-  static const std::string mock_redirect_uri(
-      "/var/www/html/new-location/index.html");
-
-  if (rand() % 2 == 0) return &mock_redirect_uri;
-  return NULL;
+const ServerConfig &ConfigAdapter::searchServerConfig(const std::string &host,
+                                                      size_t port) {
+  return Config::getInstance().getServer(0);
   (void)host;
   (void)port;
+}
+
+const LocationConfig *ConfigAdapter::searchLocationConfig(
+    const ServerConfig &server_conf, const std::string &path) {
+  return NULL;
+  (void)server_conf;
   (void)path;
 }
 
-int ConfigAdapter::searchRedirectStatusCode(const std::string &host,
-                                            size_t port,
+std::string ConfigAdapter::makeAbsolutePath(const LocationConfig &location_conf,
                                             const std::string &path) {
-  int random_number = (rand() % 6);
-  int status_code;
-  switch (random_number) {
-    case 0:
-      status_code = 301;
-      break;
-    case 1:
-      status_code = 302;
-      break;
-    case 2:
-      status_code = 303;
-      break;
-    case 3:
-      status_code = 307;
-      break;
-    case 4:
-      status_code = 308;
-      break;
-    case 5:
-      status_code = 0;
-      break;
-    default:
-      status_code = 0;
-      break;
-  }
-  return status_code;
-  (void)host;
-  (void)port;
-  (void)path;
+  return "/var/www/html/" + path;
+  (void)location_conf;
 }
 
-bool ConfigAdapter::isCgiPath(const std::string &host, size_t port,
-                              const std::string &path) {
-  return (rand() % 2 == 0);
-  (void)host;
-  (void)port;
-  (void)path;
-}
-
-const std::string *ConfigAdapter::searchErrorPage(const std::string &host,
-                                                  size_t port,
-                                                  size_t status_code) {
-  static const std::string mock_error_page("/var/www/html/error/error.html");
-
-  if (rand() % 2 == 0) return &mock_error_page;
+const std::string *ConfigAdapter::searchRedirectUri(
+    const LocationConfig &location_conf) {
   return NULL;
-  (void)host;
-  (void)port;
-  (void)status_code;
+  (void)location_conf;
 }
 
-bool ConfigAdapter::isAllowMethods(const std::string &host, size_t port,
-                                   const std::string &path,
-                                   const std::string &method) {
-  return (rand() % 2 == 0);
-  (void)host;
-  (void)port;
+int ConfigAdapter::searchRedirectStatusCode(
+    const LocationConfig &location_conf) {
+  return 301;
+  (void)location_conf;
+}
+
+bool isCgiPath(const LocationConfig &location_conf, const std::string &path) {
+  return false;
+  (void)location_conf;
   (void)path;
+}
+
+const std::string *ConfigAdapter::searchErrorPage(
+    const ServerConfig &server_conf, int code) {
+  return NULL;
+  (void)server_conf;
+  (void)code;
+}
+
+bool ConfigAdapter::isAllowMethods(const LocationConfig &location_conf,
+                                   const std::string &method) {
+  return true;
+  (void)location_conf;
   (void)method;
 }
 
 std::vector<std::string> ConfigAdapter::getAllowMethods(
-    const std::string &host, size_t port, const std::string &path) {
-  std::vector<std::string> allow_methods;
+    const LocationConfig &location_conf) {
+  std::vector<std::string> methods;
 
-  if (rand() % 2 == 0) allow_methods.push_back("GET");
-  if (rand() % 2 == 0) allow_methods.push_back("POST");
-  if (rand() % 2 == 0) allow_methods.push_back("DELETE");
-  return allow_methods;
-  (void)host;
-  (void)port;
-  (void)path;
+  methods.push_back("GET");
+  methods.push_back("DELETE");
+  methods.push_back("POST");
+  return methods;
+  (void)location_conf;
 }
 
-bool ConfigAdapter::isCorrespondingMethod(const std::string &method) {
-  return (method == "GET" || method == "POST" || method == "DELETE");
+const std::string *ConfigAdapter::searchHostName(
+    const ServerConfig &server_conf) {
+  return NULL;
+  (void)server_conf;
 }
 
-std::vector<std::string> ConfigAdapter::makeAbsolutePaths(
-    const std::string &host, size_t port, const std::string &path) {
-  const std::string base_dir("/var/www/html");
-  const std::string base_cgi_dir("/lib");
-  std::vector<std::string> absolute_paths;
-
-  if (path.compare(0, 9, "/cgi-bin/") == 0) {
-    absolute_paths.push_back(base_cgi_dir + path);
-  } else if (path == "" || path == "/") {
-    absolute_paths.push_back(base_dir + "/index.html");
-    absolute_paths.push_back(base_dir + "/app/index.html");
-    absolute_paths.push_back(base_dir + "/form/index.html");
-  } else {
-    absolute_paths.push_back(base_dir + path);
-    absolute_paths.push_back(base_dir + "/app" + path);
-    absolute_paths.push_back(base_dir + "/form" + path);
-  }
-  return absolute_paths;
-  (void)host;
-  (void)port;
+bool ConfigAdapter::isAutoindex(const LocationConfig &location_conf) {
+  return false;
+  (void)location_conf;
 }
 
-size_t ConfigAdapter::getMaxBodySize(const std::string &host, size_t port,
-                                     const std::string &path) {
-  return INTERNAL::DEFAULT_MAX_BODY_SIZE;
-  (void)host;
-  (void)port;
-  (void)path;
+std::string ConfigAdapter::searchIndex(const LocationConfig &location_conf) {
+  return "/var/www/html/index.html";
+  (void)location_conf;
+}
+
+size_t ConfigAdapter::getClientMaxBodySize(
+    const LocationConfig &location_conf) {
+  return INTERNAL::DEFAULT_MAX_CLIENT_BODY_SIZE;
+  (void)location_conf;
+}
+
+size_t ConfigAdapter::getMaxNumberOfCrlfBeforeMethod(void) {
+  return INTERNAL::DEFAULT_MAX_NUMBER_OF_CRLF_BEFORE_METHOD;
+}
+
+size_t ConfigAdapter::getMaxMethodSize(void) {
+  return INTERNAL::DEFAULT_MAX_METHOD_SIZE;
+}
+
+size_t ConfigAdapter::getMaxUriSize(void) {
+  return INTERNAL::DEFAULT_MAX_URI_SIZE;
 }
 
 size_t ConfigAdapter::getMaxHeaderSize(void) {
@@ -134,47 +109,10 @@ size_t ConfigAdapter::getMaxNumberOfHeaders(void) {
   return INTERNAL::DEFAULT_MAX_NUMBER_OF_HEADERS;
 }
 
-size_t ConfigAdapter::getMaxUriSize(void) {
-  return INTERNAL::DEFAULT_MAX_URI_SIZE;
+size_t ConfigAdapter::getMaxBodySize(void) {
+  return INTERNAL::DEFAULT_MAX_BODY_SIZE;
 }
 
-size_t ConfigAdapter::getMaxMethodSize(void) {
-  return INTERNAL::DEFAULT_MAX_METHOD_SIZE;
+bool ConfigAdapter::isCorrespondingMethod(const std::string &method) {
+  return (method == "GET" || method == "POST" || method == "DELETE");
 }
-
-size_t ConfigAdapter::getMaxNumberOfCrlfBeforeMethod(void) {
-  return INTERNAL::DEFAULT_MAX_NUMBER_OF_CRLF_BEFORE_METHOD;
-}
-
-// #include <iostream>
-// using namespace ConfigAdapter;
-// using namespace std;
-// int main(void) {
-//   srand(time(NULL));
-//   const std::string *uri = searchRedirectUri("", 0, "");
-//   cout << (uri ? *uri : "NULL") << endl;
-//   cout << searchRedirectStatusCode("", 0, "") << endl;
-//   cout << isCgiPath("", 0, "") << endl;
-//   const std::string *page = searchErrorPage("", 0, 400);
-//   cout << (page ? *page : "NULL") << endl;
-//   cout << isAllowMethods("", 0, "", "") << endl;
-//   vector<string> methods = getAllowMethods("", 0, "");
-//   cout << (1 <= methods.size() ? methods[0] : "")
-//        << (2 <= methods.size() ? ", " + methods[1] : "")
-//        << (3 <= methods.size() ? ", " + methods[2] : "");
-//   cout << endl;
-//   vector<string> paths = makeAbsolutePaths("", 0, "/");
-//   for (vector<string>::const_iterator it = paths.begin(); it != paths.end();
-//        ++it)
-//     cout << *it << " ";
-//   cout << endl;
-//   vector<string> cgi_paths = makeAbsolutePaths("", 0, "/cgi-bin/cgi.py");
-//   for (vector<string>::const_iterator it = cgi_paths.begin();
-//        it != cgi_paths.end(); ++it)
-//     cout << *it << " ";
-//   cout << endl;
-//   cout << getMaxHeaderSize() << endl;
-//   cout << getMaxUriSize() << endl;
-//   cout << getMaxBodySize("", 0, "") << endl;
-//   return 0;
-// }
