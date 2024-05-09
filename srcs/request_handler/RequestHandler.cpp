601cab319147969b77e9099e8bf84855731d7234
rikeda-cloud <longsichitian648@gmail.com>
2024-05-09 17:56:59 +0900

    [update] ConfigAdapterを使用している部分をコメントアウト

--- srcs/request_handler/RequestHandler.cpp
+++ srcs/request_handler/RequestHandler.cpp
@@ -8,6 +8,7 @@
 #include "Response.hpp"
 #include "ServerException.hpp"
 #include "Utils.hpp"
+#include "config/Config.hpp"
 #include "config/ConfigAdapter.hpp"
 
 connection::State RequestHandler::dispatch(const Request& request,
@@ -16,17 +17,19 @@ connection::State RequestHandler::dispatch(const Request& request,
       method_handler_map = MethodHandler::makeMethodHandlerMap();
   const Uri& uri = request.getRequestData()->getUri();
 
-  if (!ConfigAdapter::isAllowMethods(uri.getHost(), uri.getPort(),
-                                     uri.getPath(),
-                                     request.getRequestData()->getMethod()))
-    throw ServerException(ServerException::SERVER_ERROR_METHOD_NOT_ALLOWED,
-                          "Method not allowed");
+  // TODO LocationConfig
+  // if (!ConfigAdapter::isAllowMethods(
+  //         LocationConfig,
+  //         request.getRequestData()->getMethod()))
+  //   throw ServerException(ServerException::SERVER_ERROR_METHOD_NOT_ALLOWED,
+  //                         "Method not allowed");
 
-  const std::string* redirect_uri = ConfigAdapter::searchRedirectUri(
-      uri.getHost(), uri.getPort(), uri.getPath());
-  if (redirect_uri != NULL) {
-    return RequestHandler::redirectHandler(request, response);
-  }
+  // TODO LocationConfig
+  // const std::string* redirect_uri =
+  //     ConfigAdapter::searchRedirectUri(LocatioonConfig);
+  // if (redirect_uri != NULL) {
+  //   return RequestHandler::redirectHandler(request, response);
+  // }
   // TODO CGI Handle
   // else if (ConfigAdapter::isCgiPath(uri.getHost(), uri.getPort(),
   //   		  uri.getPath())) {
@@ -42,18 +45,23 @@ connection::State RequestHandler::dispatch(const Request& request,
 connection::State RequestHandler::redirectHandler(const Request& request,
                                                   Response& response) {
   const Uri& uri = request.getRequestData()->getUri();
+  // TODO locationConfig
   // const std::string* redirect_uri = ConfigAdapter::searchRedirectUri(
   //    uri.getHost(), uri.getPort(), uri.getPath());
-  const int redirect_status_code = ConfigAdapter::searchRedirectStatusCode(
-      uri.getHost(), uri.getPort(), uri.getPath());
+  // const int redirect_status_code = ConfigAdapter::searchRedirectStatusCode(
+  //     LocationConfig);
+
+  // (仮)
+  int redirect_status_code = 301;
 
   if (!HttpUtils::isRedirectStatusCode(redirect_status_code))
     throw ServerException(ServerException::SERVER_ERROR_INTERNAL_SERVER_ERROR,
                           "Return directive is invalid");
-  const std::string* error_page = ConfigAdapter::searchErrorPage(
-      uri.getHost(), uri.getPort(), redirect_status_code);
-  response.appendBody(HttpUtils::generateErrorPage(
-      error_page, redirect_status_code, "Redirect"));
+  // TODO LocationConfig
+  // const std::string* error_page = ConfigAdapter::searchErrorPage(
+  //     LocationConfig, redirect_status_code);
+  // response.appendBody(HttpUtils::generateErrorPage(
+  //     error_page, redirect_status_code, "Redirect"));
   response.insertContentLengthIfNotSet();
   // TODO redirect_uri use config data
   response.insertHeader("Location", "/var/www/html/new-location/index.html");
@@ -65,19 +73,25 @@ connection::State RequestHandler::errorRequestHandler(
     const Request& request, Response& response, int status_code,
     const std::string& phrase) {
   const Uri& uri = request.getRequestData()->getUri();
-  const std::string* error_page =
-      ConfigAdapter::searchErrorPage(uri.getHost(), uri.getPort(), status_code);
+  // TODO ServerConfig
+  // if (request.getServerConfig() != NULL) {
+  //   const std::string* error_page =
+  //       ConfigAdapter::searchErrorPage(request.getServerConfig(),
+  //       status_code);
+  //   response.appendBody(
+  //       HttpUtils::generateErrorPage(error_page, status_code, phrase));
+  // } else
+  response.appendBody(HttpUtils::generateErrorPage(status_code, phrase));
 
-  response.appendBody(
-      HttpUtils::generateErrorPage(error_page, status_code, phrase));
+  // (仮)
   response.insertHeader("Content-Type", "text/html");
   response.insertContentLengthIfNotSet();
   if (status_code == 405) {
     const Uri& uri = request.getRequestData()->getUri();
-    const std::vector<std::string> allow_methods =
-        ConfigAdapter::getAllowMethods(uri.getHost(), uri.getPort(),
-                                       uri.getPath());
-    response.insertHeader("Allow", Utils::joinStrings(allow_methods, " ,"));
+    // TODO LocationConfig
+    // const std::vector<std::string> allow_methods =
+    //     ConfigAdapter::getAllowMethods(LocationConfig);
+    // response.insertHeader("Allow", Utils::joinStrings(allow_methods, " ,"));
   }
   response.setStatusLine(status_code, phrase);
   return connection::SEND;
