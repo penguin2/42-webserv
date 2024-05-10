fd5ff228ee5e9d6f9c4c6f546b48661fccd8edf1
Your Name <you@example.com>
2024-05-10 00:41:14 +0000

    omit condition

--- srcs/config/TryFilesDirectiveHandler.cpp
+++ srcs/config/TryFilesDirectiveHandler.cpp
@@ -15,10 +15,6 @@ void TryFilesDirectiveHandler::setConfig() {
   for (size_t i = 1; i < this->tokens_.size() - 1; i++) {
     if (Utils::isStartsWith(tokens_[i], "=")) {
       int status_code;
-      if (tokens_[i].size() < 1) {
-        std::cout << "Status code is required after =" << std::endl;
-        exit(1);
-      }
       std::string value = tokens_[i].substr(1);
       if (!Utils::parseValue(value, status_code)) {
         std::cout << "Failed to parse as int" << std::endl;
