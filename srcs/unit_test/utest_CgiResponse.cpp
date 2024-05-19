caaab78552da2542896e6287c9b7b5b2fc8aca1a
rikeda <rikeda@penguin>
2024-05-19 14:52:08 +0900

    [fix] 単体テストでCgiResponse.parse()が終了可能かの判定を廃止

--- srcs/unit_test/utest_CgiResponse.cpp
+++ srcs/unit_test/utest_CgiResponse.cpp
@@ -5,56 +5,62 @@
 
 using namespace std;
 
-// 正常系のTEST & PARSE終了かの判定
-// (HeaderとBodyを分けるLFが存在すればパースを終了可能)
-void testParse(string cgi_raw_string, bool expect) {
+// 正常系のTEST
+void testParse(string cgi_raw_string) {
   CgiResponse cgi_response;
-  bool parse_end;
-  ASSERT_NO_THROW(parse_end = cgi_response.parse(cgi_raw_string));
-  EXPECT_EQ(parse_end, expect);
+  ASSERT_NO_THROW(cgi_response.parse(cgi_raw_string));
 }
 
 // 異常系で例外がthrowされるかをテスト
-void testParseThrowException(string cgi_raw_string) {
+void testParseError(string cgi_raw_string) {
   CgiResponse cgi_response;
   ASSERT_THROW(cgi_response.parse(cgi_raw_string), ServerException);
 }
 
 TEST(CgiResponse, PARSE) {
-  // CGI RAW STRING, EXPECT PARSE FINISH
-  testParse("\n", true);
-  testParse("Location: /index.html\n\n", true);
-  testParse("Location: http://localhost\n\n", true);
-  testParse("Location: http://localhost/?q=50#fragment\n\n", true);
-  testParse("Status: 200 OK\n\n", true);
-  testParse("Status: 999 OKOK\n\n", true);
-  testParse("Status: 444 Four Four Four\n\n", true);
-  testParse("MY-Header: MyValue\n\n", true);
-  testParse("\nBODY", true);
-  testParse("Content-Type: text/html\n\n", true);
-  testParse("Content-Type: my/type\n\n", true);
-  testParse("Content-Type: my/type; charset=UTF-8\n\n", true);
+  testParse("Location: /index.html\n\n");
+  testParse("Location: http://localhost\n\n");
+  testParse("Location: http://localhost/?q=50#fragment\n\n");
+  testParse("Status: 200 OK\n\n");
+  testParse("Status: 999 OKOK\n\n");
+  testParse("Status: 444 Four Four Four\n\n");
+  testParse("MY-Header: MyValue\n\n");
+  testParse("Content-Type: text/html\n\n");
+  testParse("Content-Type: my/type\n\n");
+  testParse("Content-Type: my/type; charset=UTF-8\n\n");
 
-  testParse("", false);
-  testParse("Location: /abc", false);
-  testParse("Location: /abc\n", false);
-  testParse("Status: 200 OK\n", false);
-  testParse("MY-Header: MyValue\n", false);
+  // 改行のみの場合はCgiResponseHandlerでエラーになります
+  testParse("\n");
+  testParse("\nBODY");
+  testParse("");
+  testParse("Location: /abc");
+  testParse("Location: /abc\n");
+  testParse("Status: 200 OK\n");
+  testParse("MY-Header: MyValue\n");
+  testParse(" MY-Header: MyValue\n");
 }
 
 TEST(CgiResponse, ParseThrowServerException) {
-  // CGI RAW STRING
-  testParseThrowException("Location: 1\n\n");
-  testParseThrowException("Location: http://\n\n");
-  testParseThrowException("Location: http://localhost/../index.html\n\n");
-  testParseThrowException("Location: /../index.html\n\n");
-  testParseThrowException("Status: 3333 ERROR\n\n");
-  testParseThrowException("Status: 33 ERROR\n\n");
-  testParseThrowException("Status: ERROR\n\n");
-  testParseThrowException("Status: 200\n\n");
-  testParseThrowException("Status: 200 \n\n");
-  testParseThrowException("Status: 200 \n\nBODY");
-  testParseThrowException("Content-Type: text \n\n");
-  testParseThrowException("Content-Type: text/ \n\n");
-  testParseThrowException("Content-Type:  /html \n\n");
+  testParseError("MyHeader\n\n");
+  testParseError(":MyValue\n\n");
+  testParseError(":\n\n");
+  testParseError("MyHeader : MyValue\n\n");
+  // LocationHeaderのフォーマットは正しくなくてはならない
+  testParseError("Location: 1\n\n");
+  testParseError("Location: http://\n\n");
+  testParseError("Location: /../index.html\n\n");
+  testParseError("Location: http://localhost/../index.html\n\n");
+  // 課題上、HTTPSに対応しないのでリダイレクト先もHTTPSに対応しない
+  testParseError("Location: https://localhost/\n\n");
+  // StatusHeaderのフォーマットは正しくなくてはならない
+  testParseError("Status: 3333 ERROR\n\n");
+  testParseError("Status: 33 ERROR\n\n");
+  testParseError("Status: ERROR\n\n");
+  testParseError("Status: 200\n\n");
+  testParseError("Status: 200 \n\n");
+  testParseError("Status: 200 \n\nBODY");
+  // Content-Typeのフォーマットは正しくなくてはならない
+  testParseError("Content-Type: text \n\n");
+  testParseError("Content-Type: text/ \n\n");
+  testParseError("Content-Type:  /html \n\n");
 }
--- srcs/unit_test/utest_CgiResponseHandler.cpp
+++ srcs/unit_test/utest_CgiResponseHandler.cpp
@@ -26,17 +26,18 @@ class RequestMock {
   ServerConfig* server_conf_;
 };
 
+// 正常系のTEST
 void testConvertCgi2Http(string cgi_response_string) {
   RequestMock request;
   CgiResponse cgi_response;
-  ASSERT_NO_THROW(cgi_response.parse(cgi_response_string))
-      << cgi_response_string;
+  ASSERT_NO_THROW(cgi_response.parse(cgi_response_string));
 
   ASSERT_NO_THROW(CgiResponseHandler::convertCgiResponseDataToHttpResponseData(
       request.getRequest(), cgi_response.getResponseData()));
 }
 
-void testConvertCgi2HttpFailure(string cgi_response_string) {
+// 異常系で例外がthrowされるかをテスト
+void testConvertCgi2HttpError(string cgi_response_string) {
   RequestMock request;
   CgiResponse cgi_response;
   ASSERT_NO_THROW(cgi_response.parse(cgi_response_string));
@@ -48,21 +49,64 @@ void testConvertCgi2HttpFailure(string cgi_response_string) {
 
 TEST(CgiResponseHandler, ConvertCgi2Http) {
   // DocumentResonse
+  testConvertCgi2Http("Content-Type: text/html\n\nabc");
   testConvertCgi2Http("Content-Type: text/html\nStatus: 200 FFFF\n\nabc");
+  testConvertCgi2Http("Content-Type: text/html\nStatus: 999 QQQQ\n\nabc");
+  testConvertCgi2Http("MyHeader:1\nContent-Type: text/html\n\nabc");
   // LocalRedirectResponse
   testConvertCgi2Http("Location: /\n\n");
   testConvertCgi2Http("Location: /././././index.html\n\n");
-  testConvertCgi2Http("Location: /aaa/../index.html\n\n");
+  testConvertCgi2Http("location: /aaa/../index.html\n\n");
+  testConvertCgi2Http("location: /DIR/\n\n");
+  testConvertCgi2Http("location: /index.html?q=50&c=%20#fragment\n\n");
+  testConvertCgi2Http("location: /DIR/\n MyHeder:1\n\n");
   // ClientRedirectResponse
   testConvertCgi2Http("Location: http://localhost/index.html\n\n");
   testConvertCgi2Http("Location: http://localhost/?q=50\n\n");
   testConvertCgi2Http("Location: http://localhost/?c=%20#fragment\n\n");
+  testConvertCgi2Http("Location: http://youser:pass@localhost/\n\n");
   // ClientRedirectResponseWithDocument
+  testConvertCgi2Http(
+      "Location: http://localhost/index.html\nStatus:302 "
+      "F\nContent-Type:text/html\n\nabc");
+  testConvertCgi2Http(
+      "Location: http://localhost/?q=50\nStatus:302 "
+      "F\nContent-Type:a/b\n\nabc");
+  testConvertCgi2Http(
+      "LOCATION: "
+      "http://localhost/?c=%20#fragment\nsTATUS:302 "
+      "Found\ncontent-type:1/2\n\nabc");
+  testConvertCgi2Http(
+      "status:302 FOUND\ncontent-type:a/a; charset=UTF=8\nLocation: "
+      "http://youser:pass@localhost/\n\nabc");
 }
 
-TEST(CgiResponseHandler, ConvertCgi2HttpFailure) {
-  testConvertCgi2HttpFailure("");
-  testConvertCgi2HttpFailure("ABC: ABC\n\n");
-  testConvertCgi2HttpFailure("Location: /\nA:a\n\n");
-  testConvertCgi2HttpFailure("Location: http://localhost/\nMyHeader:1\n\n");
+TEST(CgiResponseHandler, ConvertCgi2HttpError) {
+  testConvertCgi2HttpError("");
+  testConvertCgi2HttpError("\n");
+  testConvertCgi2HttpError("\n\n");
+  testConvertCgi2HttpError("ABC: ABC\n\n");
+  testConvertCgi2HttpError(" Location: /\n\n");
+  // ONLY Status
+  testConvertCgi2HttpError("Status: 200 OK\n\n");
+  // Status and BODY
+  testConvertCgi2HttpError("Status: 200 OK\n\nabc");
+  // Location and Other-Header
+  testConvertCgi2HttpError("Location: /\nA:a\n\n");
+  testConvertCgi2HttpError("Location: http://localhost/\nA:a\n\n");
+  // Location and BODY
+  testConvertCgi2HttpError("Location: /\n\nabc");
+  testConvertCgi2HttpError("Location: http://localhost/\n\nabc");
+  // ONLY Content-Type
+  testConvertCgi2HttpError("Content-Type: text/html\n\n");
+  // Status and Location
+  testConvertCgi2HttpError("Location:/\nStatus: 200 OK\n\n");
+  testConvertCgi2HttpError("Location:http://abc/\nStatus: 200 OK\n\n");
+  // Status and Location and BODY
+  testConvertCgi2HttpError("Location:/\nStatus: 200 OK\n\nabc");
+  testConvertCgi2HttpError("Location:http://abc/\nStatus: 200 OK\n\nabc");
+  // Location and Content-Type
+  testConvertCgi2HttpError("Location: /\nContent-Type: text/html\n\n");
+  // Location and Content-Type and BODY
+  testConvertCgi2HttpError("Location: /\nContent-Type: text/html\n\nabc");
 }
