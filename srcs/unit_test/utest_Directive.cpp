de280cb1690c31137d8fcc6bac2e520f116adee9
rikeda <rikeda@penguin>
2024-05-20 14:50:22 +0900

    [add] ServerNameDirectiveHandler unit test

--- srcs/unit_test/utest_Directive.cpp
+++ srcs/unit_test/utest_Directive.cpp
@@ -4,6 +4,7 @@
 #include "config/AutoIndexDirectiveHandler.hpp"
 #include "config/CgiExtDirectiveHandler.hpp"
 #include "config/ConfigParser.hpp"
+#include "config/ServerNameDirectiveHandler.hpp"
 
 static std::vector<std::string> tempTokenize(const std::string& line) {
   std::vector<std::string> tokens;
@@ -47,21 +48,22 @@ static void testIsValid(const std::string& line, bool expected) {
   T directive_handler;
   directive_handler.setToken(tempTokenize(line));
   if (directive_handler.isSyntaxValid()) {
-    EXPECT_EQ(directive_handler.isDirectiveValid(), expected) << "testing... " << line;
+    EXPECT_EQ(directive_handler.isDirectiveValid(), expected)
+        << "testing... " << line;
   }
 }
 
 TEST(Directive, AllowMethodsDirectiveHandler) {
   typedef AllowMethodsDirectiveHandler DirectiveHandler;
 
-  //true
+  // true
   testIsValid<DirectiveHandler>("allow_methods GET;", true);
   testIsValid<DirectiveHandler>("allow_methods POST;", true);
   testIsValid<DirectiveHandler>("allow_methods DELETE;", true);
   testIsValid<DirectiveHandler>("allow_methods DELETE GET;", true);
   testIsValid<DirectiveHandler>("allow_methods GET POST DELETE;", true);
 
-  //false
+  // false
   testIsValid<DirectiveHandler>("", false);
   testIsValid<DirectiveHandler>("allow_methods get;", false);
   testIsValid<DirectiveHandler>("allow_methods post;", false);
@@ -71,7 +73,7 @@ TEST(Directive, AllowMethodsDirectiveHandler) {
   testIsValid<DirectiveHandler>("allow_methods head;", false);
   testIsValid<DirectiveHandler>("allow_methods HEAD;", false);
   testIsValid<DirectiveHandler>("allow_methods get POST DELETE;", false);
-  
+
   testIsValid<DirectiveHandler>("allow_methods GET GET POST DELETE;", false);
 
   testIsValid<DirectiveHandler>("allow_methods GET; POST DELETE;", false);
@@ -90,10 +92,34 @@ TEST(Directive, CgiExtDirectiveHandler) {
   testIsValid<DirectiveHandler>("", true);
 }
 
+TEST(Directive, serverNameDirectiveHandler) {
+  typedef ServerNameDirectiveHandler DirectiveHandler;
+
+  testIsValid<DirectiveHandler>("server_name 1;", true);
+  testIsValid<DirectiveHandler>("server_name localhost;", true);
+  testIsValid<DirectiveHandler>("server_name 0.0.0.0;", true);
+  testIsValid<DirectiveHandler>("server_name 100.115.92.194;", true);
+  testIsValid<DirectiveHandler>("server_name '';", true);
+  testIsValid<DirectiveHandler>("server_name a_b-c~d;", true);
+  testIsValid<DirectiveHandler>("server_name !a$b&c(d)*e+f,=;", true);
+
+  testIsValid<DirectiveHandler>("server_name ", false);
+  testIsValid<DirectiveHandler>("server_name ;", false);
+  testIsValid<DirectiveHandler>("server_name localhost 8080;", false);
+  testIsValid<DirectiveHandler>("server_name google.com/abc;", false);
+  testIsValid<DirectiveHandler>("server_name google.%20com;", false);
+  testIsValid<DirectiveHandler>("server_name google.com#abc;", false);
+  testIsValid<DirectiveHandler>("server_name localhost:80;", false);
+  testIsValid<DirectiveHandler>("server_name youser:pass@localhost;", false);
+  testIsValid<DirectiveHandler>("server_name http://google.com;", false);
+  testIsValid<DirectiveHandler>("server_name \"\";", false);
+}
+
 // ...
 //
 // TEST(Directive, 'SomeDirectiveHandler') {
 //   typedef 'SomeDirectiveHandler' DirectiveHandler;
 //
-//   testIsValid<DirectiveHandler>('line_to_test', 'true if "line_to_test" is valid else false');
+//   testIsValid<DirectiveHandler>('line_to_test', 'true if "line_to_test" is
+//   valid else false');
 // }
