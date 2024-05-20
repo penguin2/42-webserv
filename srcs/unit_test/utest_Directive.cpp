ae674af9bddae920403d56cea2b30cfc883693c3
rikeda <rikeda@penguin>
2024-05-20 22:22:17 +0900

    [add] ReturnDirectiveHandler unit test

--- srcs/unit_test/utest_Directive.cpp
+++ srcs/unit_test/utest_Directive.cpp
@@ -4,6 +4,7 @@
 #include "config/AutoIndexDirectiveHandler.hpp"
 #include "config/CgiExtDirectiveHandler.hpp"
 #include "config/ConfigParser.hpp"
+#include "config/ReturnDirectiveHandler.hpp"
 
 static std::vector<std::string> tempTokenize(const std::string& line) {
   std::vector<std::string> tokens;
@@ -47,21 +48,22 @@ static void testIsValid(const std::string& line, bool expected) {
   T directive_handler;
   directive_handler.setToken(tempTokenize(line));
   if (directive_handler.isSyntaxValid()) {
-    EXPECT_EQ(directive_handler.isDirectiveValid(), expected) << "testing... " << line;
+    EXPECT_EQ(directive_handler.isDirectiveValid(), expected)
+        << "testing... " << line;
   }
 }
 
 TEST(Directive, AllowMethodsDirectiveHandler) {
   typedef AllowMethodsDirectiveHandler DirectiveHandler;
 
-  //true
+  // true
   testIsValid<DirectiveHandler>("allow_methods GET;", true);
   testIsValid<DirectiveHandler>("allow_methods POST;", true);
   testIsValid<DirectiveHandler>("allow_methods DELETE;", true);
   testIsValid<DirectiveHandler>("allow_methods DELETE GET;", true);
   testIsValid<DirectiveHandler>("allow_methods GET POST DELETE;", true);
 
-  //false
+  // false
   testIsValid<DirectiveHandler>("", false);
   testIsValid<DirectiveHandler>("allow_methods get;", false);
   testIsValid<DirectiveHandler>("allow_methods post;", false);
@@ -71,7 +73,7 @@ TEST(Directive, AllowMethodsDirectiveHandler) {
   testIsValid<DirectiveHandler>("allow_methods head;", false);
   testIsValid<DirectiveHandler>("allow_methods HEAD;", false);
   testIsValid<DirectiveHandler>("allow_methods get POST DELETE;", false);
-  
+
   testIsValid<DirectiveHandler>("allow_methods GET GET POST DELETE;", false);
 
   testIsValid<DirectiveHandler>("allow_methods GET; POST DELETE;", false);
@@ -90,10 +92,45 @@ TEST(Directive, CgiExtDirectiveHandler) {
   testIsValid<DirectiveHandler>("", true);
 }
 
+TEST(Directive, ReturnDirectiveHandler) {
+  typedef ReturnDirectiveHandler DirectiveHandler;
+
+  testIsValid<DirectiveHandler>("return 301 /;", true);
+  testIsValid<DirectiveHandler>("return 302 /index.html;", true);
+  testIsValid<DirectiveHandler>("return 303 /abc/def;", true);
+  testIsValid<DirectiveHandler>("return 307 http://localhost/;", true);
+  testIsValid<DirectiveHandler>("return 308 http://abc/?q=50#fragment;", true);
+  testIsValid<DirectiveHandler>("return 301 /././index.html;", true);
+  testIsValid<DirectiveHandler>("return 302 /abc/../index.html;", true);
+  testIsValid<DirectiveHandler>("return 303 http://localhost/;", true);
+  testIsValid<DirectiveHandler>("return 307 http://youser:pass@abc/;", true);
+  testIsValid<DirectiveHandler>("return 308 ////////;", true);
+
+  testIsValid<DirectiveHandler>("return ", false);
+  testIsValid<DirectiveHandler>("return ;", false);
+  testIsValid<DirectiveHandler>("return 301;", false);
+  testIsValid<DirectiveHandler>("return /;", false);
+  testIsValid<DirectiveHandler>("return -301 /;", false);
+  testIsValid<DirectiveHandler>("return 301 /index.html OK;", false);
+  testIsValid<DirectiveHandler>("return 18446744073709551317 /;", false);
+  testIsValid<DirectiveHandler>("return 301 ../;", false);
+  testIsValid<DirectiveHandler>("return 301 /../root;", false);
+  testIsValid<DirectiveHandler>("return 301 http:///abc;", false);
+  testIsValid<DirectiveHandler>("return 301 http:///abc;", false);
+  testIsValid<DirectiveHandler>("return 300 /;", false);
+  testIsValid<DirectiveHandler>("return 304 /;", false);
+  testIsValid<DirectiveHandler>("return 305 /;", false);
+  testIsValid<DirectiveHandler>("return /abc 301;", false);
+
+  // HTTPSは対応しないためredirect先としても認めない
+  testIsValid<DirectiveHandler>("return 308 https://localhost/new", false);
+}
+
 // ...
 //
 // TEST(Directive, 'SomeDirectiveHandler') {
 //   typedef 'SomeDirectiveHandler' DirectiveHandler;
 //
-//   testIsValid<DirectiveHandler>('line_to_test', 'true if "line_to_test" is valid else false');
+//   testIsValid<DirectiveHandler>('line_to_test', 'true if "line_to_test" is
+//   valid else false');
 // }
