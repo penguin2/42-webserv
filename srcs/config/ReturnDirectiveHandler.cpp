9b6e55f0c76c5747886065deb5942f43b2467fb5
rikeda <rikeda@penguin>
2024-05-20 22:21:56 +0900

    [update] ReturnDirectiveHandler

--- srcs/config/ReturnDirectiveHandler.cpp
+++ srcs/config/ReturnDirectiveHandler.cpp
@@ -1,24 +1,41 @@
 #include "./config/ReturnDirectiveHandler.hpp"
 
+#include "HttpUtils.hpp"
+#include "ServerException.hpp"
+#include "Uri.hpp"
+
 ReturnDirectiveHandler::ReturnDirectiveHandler() {
   this->directive_context_ = ConfigEnums::LOCATION;
 }
 
 bool ReturnDirectiveHandler::isDirectiveValid() const {
-  (void)tokens_;
+  if (tokens_.size() != 4) {
+    return false;
+  }
+
+  size_t status_code;
+  const std::string& status_code_string = tokens_[1];
+  const std::string& uri_string = tokens_[2];
+
+  try {
+    Uri uri;
+    uri.parse(uri_string);
+  } catch (ServerException& e) {
+    return false;
+  }
+
+  if (Utils::strToSize_t(status_code_string, status_code, 10) == false ||
+      1000 <= status_code || !HttpUtils::isRedirectStatusCode(status_code))
+    return false;
   return true;
 }
 
 void ReturnDirectiveHandler::setConfig() {
   log();
   LocationConfig& locationConfig = getLocationConfig();
-  int return_status_code;
-  if (!Utils::parseValue(tokens_[1], return_status_code)) {
-    std::cout << "Failed to parse as int" << std::endl;
-    exit(1);
-  }
+  size_t return_status_code;
+  Utils::strToSize_t(tokens_[1], return_status_code, 10);
 
   locationConfig.setReturnStatusCode(return_status_code);
-
   locationConfig.setReturnUri(tokens_[2]);
 }
