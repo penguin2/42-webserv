f9c80b825529e3bafaf13cf631fd9e2f7b59af03
rikeda <rikeda@penguin>
2024-05-20 21:45:11 +0900

    [update] AutoIndexDirectiveHandler

--- srcs/config/AutoIndexDirectiveHandler.cpp
+++ srcs/config/AutoIndexDirectiveHandler.cpp
@@ -1,16 +1,22 @@
 #include "./config/AutoIndexDirectiveHandler.hpp"
 
+#include "Utils.hpp"
+
 AutoIndexDirectiveHandler::AutoIndexDirectiveHandler() {
   this->directive_context_ = ConfigEnums::LOCATION;
 }
 
 bool AutoIndexDirectiveHandler::isDirectiveValid() const {
-  (void)tokens_;
+  if (tokens_.size() != 3) {
+    return false;
+  }
+  std::string on_or_off = Utils::toLower(tokens_[1]);
+  if (on_or_off != "on" && on_or_off != "off") return false;
   return true;
 }
 
 void AutoIndexDirectiveHandler::setConfig() {
   log();
   LocationConfig& locationConfig = getLocationConfig();
-  locationConfig.setAutoindex(tokens_[1] == "on");
+  locationConfig.setAutoindex(Utils::toLower(tokens_[1]) == "on");
 }
