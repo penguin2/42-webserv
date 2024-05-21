fdd11c4c6c68ec6cfb34d3ce29a8059a2ef088a2
rikeda-cloud <longsichitian648@gmail.com>
2024-05-21 13:55:02 +0900

    [update] IndexDirectiveHandler

--- srcs/config/IndexDirectiveHandler.cpp
+++ srcs/config/IndexDirectiveHandler.cpp
@@ -1,11 +1,20 @@
 #include "./config/IndexDirectiveHandler.hpp"
 
+#include "UriUtils.hpp"
+#include "Utils.hpp"
+
 IndexDirectiveHandler::IndexDirectiveHandler() {
   this->directive_context_ = ConfigEnums::LOCATION;
 }
 
 bool IndexDirectiveHandler::isDirectiveValid() const {
-  (void)tokens_;
+  if (tokens_.size() != 3) {
+    return false;
+  }
+  const std::string& index_path = tokens_[1];
+  if (!Utils::isContainsOnly(index_path,
+                             UriUtils::isPathCharsetWithoutPctEncoding))
+    return false;
   return true;
 }
 
