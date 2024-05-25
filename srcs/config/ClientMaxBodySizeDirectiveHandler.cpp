b835408a8b2d4e6ab30c9fde3a1ddab166709b1c
Your Name <you@example.com>
2024-05-26 01:30:16 +0900

    fix way to use strToSize_t

--- srcs/config/ClientMaxBodySizeDirectiveHandler.cpp
+++ srcs/config/ClientMaxBodySizeDirectiveHandler.cpp
@@ -28,10 +28,11 @@ void ClientMaxBodySizeDirectiveHandler::setConfig() {
   if (!tokens_[1].empty()) {
     tokens_[1].pop_back();
   }
-  size_t num;
-  if (Utils::strToSize_t(tokens_[1], num * 100000, 10) == false) {
+  size_t client_max_body_size_bytes;
+  if (Utils::strToSize_t(tokens_[1], client_max_body_size_bytes, 10) == false) {
     std::cerr << "Failed to strToSize_t" << tokens_[1] << std::endl;
     exit(1);
   }
-  locationConfig.setClientMaxBodySize(num);
+  size_t client_max_body_size_mega_bytes = (client_max_body_size_bytes * 1000000);
+  locationConfig.setClientMaxBodySize(client_max_body_size_mega_bytes);
 }
