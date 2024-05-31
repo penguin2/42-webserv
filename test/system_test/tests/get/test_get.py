627f3b5cd5c49ad4679e638c5bbcc1d2bc0c89a5
rikeda <rikeda@penguin>
2024-05-31 19:04:52 +0900

    [refactor] GETリクエストのテストにをconftest.pyを導入

--- test/system_test/tests/get/test_get.py
+++ test/system_test/tests/get/test_get.py
@@ -1,20 +1,4 @@
-import requests
-import os
-
-BASE_URL = "http://127.0.0.1:4242"
-
-
-def request_by_get(path: str, expect_status_code: int, expect_body: str):
-    """
-    基本的なGETメソッドの挙動をテスト
-    StatusCode・Body・Content-Lengthのdiffをテスト
-    BASE_URLは固定にしているのでPathを変えてテストを書いてください
-    """
-    url = BASE_URL + path
-    res = requests.get(url)
-    assert res.status_code == expect_status_code
-    assert res.text == expect_body
-    assert int(res.headers["Content-Length"]) == len(expect_body)
+from conftest import request_by_get, get_file_without_permission
 
 
 def test_simple_get_success():
@@ -37,7 +21,4 @@ def test_simple_get_error():
 
 
 def test_no_permission():
-    file_path = "./tests/get/html/index.html"
-    os.chmod(file_path, 0o000)
-    request_by_get("/index.html", 404, "ERROR")
-    os.chmod(file_path, 0o755)
+    get_file_without_permission("/index.html", 404, "ERROR")
