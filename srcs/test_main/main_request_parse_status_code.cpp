be059920497a0162c2b26d8a92ae1e4a3f199745
rikeda <rikeda@penguin>
2024-05-31 13:07:54 +0900

    [add] make request_parse_status_code_testによってHTTPパース時に返されるエラーコードの詳細なテストを実行可能

---
+++ srcs/test_main/main_request_parse_status_code.cpp
@@ -0,0 +1,50 @@
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <map>
+
+#include "Request.hpp"
+#include "ServerException.hpp"
+#include "Utils.hpp"
+
+static std::string getAllChars(const char* file) {
+  std::fstream fs(file);
+  std::string all_chars;
+  std::string line;
+
+  while (std::getline(fs, line)) {
+    all_chars += line;
+    if (fs.eof() == false) all_chars += std::string("\r\n");
+  }
+  return all_chars;
+}
+
+// 0 -> 異常なリクエストで例外をthrow, expect_status_codeと例外のcode()が異なる
+// 1 -> 異常なリクエストで例外をthrow, expect_status_codeと例外のcode()が一致
+// 2 -> リクエストが未完成
+// 3 -> 引数が足りていない
+// 4 -> 正常なリクエスト
+int main(int argc, char** argv) {
+  if (argc < 3) {
+    return 3;
+  }
+
+  Request request;
+  std::string buffer;
+  std::string raw_data = getAllChars(argv[1]);
+
+  size_t expect_status_code;
+  Utils::strToSize_t(argv[2], expect_status_code, 10);
+
+  try {
+    for (std::string::const_iterator it = raw_data.begin();
+         it != raw_data.end(); ++it) {
+      buffer += *it;
+      if (request.parse(buffer) == true) return 4;
+    }
+  } catch (ServerException& e) {
+    std::cout << e.code() << e.what() << std::endl;
+    return e.code() == expect_status_code;
+  }
+  return 2;
+}
