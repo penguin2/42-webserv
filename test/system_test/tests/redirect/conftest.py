5a243fb99a7af5d5ef625c216af820c1296b81ed
rikeda <rikeda@penguin>
2024-06-04 23:55:25 +0900

    [add] redirect with internal redirect

--- test/system_test/tests/redirect/conftest.py
+++ test/system_test/tests/redirect/conftest.py
@@ -7,6 +7,8 @@ BASE_URL = "http://127.0.0.1:4242"
 HTML_DIR = "./html"
 REDIRECT_SUCCESS_DIR = HTML_DIR + "/success"
 REDIRECT_ERROR_DIR = HTML_DIR + "/error"
+INTERNAL_REDIRECT_SUCCESS_DIR = HTML_DIR + "/internal_redirect_success"
+INTERNAL_REDIRECT_ERROR_DIR = HTML_DIR + "/internal_redirect_error"
 
 
 def mkdir_and_touch(directory: str, file: str):
@@ -18,12 +20,14 @@ def mkdir_and_touch(directory: str, file: str):
 @pytest.fixture(scope="session", autouse=True)
 def manage_html_directory():
     """
-    前処理で必要なディレクトリの作成
+    前処理で必要なディレクトリとファイルの作成
     後処理でディレクトリの削除
     """
     os.mkdir(HTML_DIR)
     mkdir_and_touch(REDIRECT_SUCCESS_DIR, "/index.html")
     mkdir_and_touch(REDIRECT_ERROR_DIR, "/error.html")
+    os.mkdir(INTERNAL_REDIRECT_SUCCESS_DIR)
+    os.mkdir(INTERNAL_REDIRECT_ERROR_DIR)
     yield
     if os.path.exists(HTML_DIR):
         shutil.rmtree(HTML_DIR)
