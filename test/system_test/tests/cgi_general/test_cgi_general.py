09f75b57815379680cd15dc2652bf25bc188886c
rikeda <rikeda@penguin>
2024-06-11 21:39:00 +0900

    [add] _wrap_check_cgi_meta_data_by_all_methods

--- test/system_test/tests/cgi_general/test_cgi_general.py
+++ test/system_test/tests/cgi_general/test_cgi_general.py
@@ -1,6 +1,15 @@
 from conftest import check_cgi_meta_data
 
 
+def _wrap_check_cgi_meta_data_by_all_methods(
+        path: str,
+        request_body: str = "",
+        headers: dict[str, str] = {}):
+    check_cgi_meta_data("get", path, request_body, headers)
+    check_cgi_meta_data("post", path, request_body, headers)
+    check_cgi_meta_data("delete", path, request_body, headers)
+
+
 def test_cgi_meta_vars():
     check_cgi_meta_data("get", "/meta_var_get.py")
     check_cgi_meta_data("post", "/meta_var_post.py")
@@ -8,33 +17,24 @@ def test_cgi_meta_vars():
 
 
 def test_query_string():
-    check_cgi_meta_data("get", "/query_string1.py?q=50")
-    check_cgi_meta_data("post", "/query_string1.py?q=50")
-    check_cgi_meta_data("delete", "/query_string1.py?q=50")
-
-    check_cgi_meta_data("get", "/query_string2.py?space=%20")
-    check_cgi_meta_data("get", "/query_string2.py?space= ")
+    _wrap_check_cgi_meta_data_by_all_methods("/query_string1.py?q=50")
+    _wrap_check_cgi_meta_data_by_all_methods("/query_string2.py?space=%20")
+    _wrap_check_cgi_meta_data_by_all_methods("/query_string2.py?space= ")
 
 
 def test_path_info_and_translated():
-    check_cgi_meta_data("get", "/path_info.py/abc")
-    check_cgi_meta_data("post", "/path_info.py/abc")
-    check_cgi_meta_data("delete", "/path_info.py/abc")
-
-    check_cgi_meta_data("get", "/path_info_and_query.py/123?p=1")
+    _wrap_check_cgi_meta_data_by_all_methods("/path_info.py/abc")
+    _wrap_check_cgi_meta_data_by_all_methods("/path_info_and_query.py/123?p=1")
 
 
 def test_cgi_body():
     headers = {"Content-length": "4", "content-TYPE": "text/html"}
-    check_cgi_meta_data("get", "/check_body.py", "BODY", headers)
-    check_cgi_meta_data("post", "/check_body.py", "BODY", headers)
-    check_cgi_meta_data("delete", "/check_body.py", "BODY", headers)
+    _wrap_check_cgi_meta_data_by_all_methods(
+        "/check_body.py", request_body="BODY", headers=headers)
 
 
 def test_current_dir():
-    check_cgi_meta_data("get", "/check_current_dir.py")
-    check_cgi_meta_data("post", "/check_current_dir.py")
-    check_cgi_meta_data("delete", "/check_current_dir.py")
+    _wrap_check_cgi_meta_data_by_all_methods("/check_current_dir.py")
 
 
 def test_internal_redirect():
@@ -48,6 +48,5 @@ def test_http_headers():
         "My_headeR2": "DEF",
         "HTTP_MY_HEADER3": ""
     }
-    check_cgi_meta_data("get", "/http_headers.py", headers=headers)
-    check_cgi_meta_data("post", "/http_headers.py", headers=headers)
-    check_cgi_meta_data("delete", "/http_headers.py", headers=headers)
+    _wrap_check_cgi_meta_data_by_all_methods(
+        "/http_headers.py", headers=headers)
